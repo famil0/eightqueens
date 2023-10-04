af134acc8d8ce8a781f1b00ac1fa8ad02a615b02
famil0 <103111170+famil0@users.noreply.github.com>
2023-10-04 14:31:39 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ class Queen {
     }
 
     reset() {
-        this.i = 8;
+        this.i = n;
         this.placeOrRemove();
     }
 }
@@ -27,18 +27,24 @@ startBtn.addEventListener('click', () => {
     if (running) return;
     toggle(startBtn);
     end = false;
-    if (queens[0].i == 8 && queens[7].i == 8) {
+    if (queens[0].i == n && queens[n - 1].i == n) {
         queenIdx = 0;
         backtrack();
         foundSolutions = 0;
     }
     else {
-        queenIdx = 7;
+        queenIdx = n - 1;
         backtrack();
     }
 });
 const waitCheckbox = document.querySelector('#cbwait');
 const milliseconds = document.querySelector('.ms').children[0];
+const queensNumber = document.querySelector('.queensnumber').children[0];
+queensNumber.addEventListener('input', () => {
+    if (queensNumber.innerHTML == '') return;
+    n = parseInt(queensNumber.innerHTML);
+    newBoard();
+});
 
 function toggle(element) {
     element.classList.toggle('disabled');
@@ -46,17 +52,9 @@ function toggle(element) {
 
 let board = [];
 let rows = document.querySelectorAll('.row');
-for (let i = 0; i < rows.length; i++) {
-    board[i] = [];
-    for (let j = 0; j < 8; j++) {
-        board[i][j] = rows[i].children[j];
-    }
-}
+let n = parseInt(queensNumber.innerHTML);
 let queens = [];
-for (let i = 0; i < 8; i++) {
-    queens[i] = new Queen(8, i);
-    queens[i].placeOrRemove();
-}
+newBoard();
 
 let queenIdx = 0;
 let end = false;
@@ -75,49 +73,70 @@ async function backtrack() {
         }
         else if (goodPosition(queen.i, queen.j)) {
             queenIdx++;
-            if (queenIdx == 8) {
+            if (queenIdx == n) {
                 end = true;
                 foundSolutions++;
             }
         }
-        if (queens[0].i == 8 && queens[7].i == 8) {
+        if (queens[0].i == n && queens[n - 1].i == n) {
             break;
         }
     }
     running = false;
-     toggle(startBtn);
+    toggle(startBtn);
 }
 
 
 function goodPosition(i, j) {
-    for (let k = 0; k < 8; k++) {
+    for (let k = 0; k < n; k++) {
         if (i == k) continue;
         if (board[k][j].classList.contains('queen')) return false;
     }
-    for (let k = 0; k < 8; k++) {
+    for (let k = 0; k < n; k++) {
         if (j == k) continue;
         if (board[i][k].classList.contains('queen')) return false;
     }
-    for (let k = 1; k < 8; k++) {
-        if (i + k >= 8 || j + k >= 8) break;
+    for (let k = 1; k < n; k++) {
+        if (i + k >= n || j + k >= n) break;
         if (board[i + k][j + k].classList.contains('queen')) return false;
     }
-    for (let k = 1; k < 8; k++) {
+    for (let k = 1; k < n; k++) {
         if (i - k < 0 || j - k < 0) break;
         if (board[i - k][j - k].classList.contains('queen')) return false;
     }
-    for (let k = 1; k < 8; k++) {
-        if (i + k >= 8 || j - k < 0) break;
+    for (let k = 1; k < n; k++) {
+        if (i + k >= n || j - k < 0) break;
         if (board[i + k][j - k].classList.contains('queen')) return false;
     }
-    for (let k = 1; k < 8; k++) {
-        if (i - k < 0 || j + k >= 8) break;
+    for (let k = 1; k < n; k++) {
+        if (i - k < 0 || j + k >= n) break;
         if (board[i - k][j + k].classList.contains('queen')) return false;
     }
     return true;
 }
 
+function newBoard() {
+    document.querySelector(':root').style.setProperty('--N', n);
+    if (queens.length > 0) {
+        queens.forEach(q => {
+            q.placeOrRemove();
+        });
+    }
+    board = [];
+    for (let i = 8 - n; i <= 8; i++) {
+        board[i - (8 - n)] = [];
+        for (let j = 0; j < n; j++) {
+            board[i - (8 - n)][j] = rows[i].children[j];
+        }
+    }
+    queens = [];
+    for (let i = 0; i < n; i++) {
+        queens[i] = new Queen(n, i);
+        queens[i].placeOrRemove();
+    }
+}
+
 function placeOrRemoveQueen(i, j) {
     if (i < 0) return;
-    board[i][j].classList.toggle('queen');
+     board[i][j].classList.toggle('queen');
 }
